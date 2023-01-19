c13fae416b4e300203a1c1cf5053ccb5c4cbec79
dzanaf <dzana.feratovic@klika.ba>
2023-01-19 15:39:08 +0100

    [CON-768] NL translations

---
+++ views/admin/nl/nl_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$sLangName = 'Dutch';
+
+$aLang = array(
+    'charset'                            => 'UTF-8',
+    'SHOP_MODULE_GROUP_oemondu_banners'  => 'Module-instellingen',
+    'SHOP_MODULE_oemonduApiKey'          => 'API Key',
+    'SHOP_MODULE_oemonduSandboxMode'     => 'Sandbox mode',
+    'SHOP_MODULE_oemonduErrorLogging'    => 'Activeer Mondu-loggen',
+
+    'INVALID_API_KEY'              => 'Ongeldige Mondu API-sleutel.',
+    'MONDU_WILL_CREATE_INVOICE'    => 'Als deze bestelling wordt verzonden, wordt er een factuur in Mondu gemaakt. Wil je doorgaan?',
+    'MONDU_WILL_CANCEL_INVOICE'    => 'Als u de verzending van de bestelling annuleert, wordt een factuur in Mondu geannuleerd. Deze actie kan niet worden teruggedraaid. Weet je zeker dat je verder wilt gaan?',
+    'MONDU_CREATE_INVOICE_ERROR'   => 'Deze bestelling kan niet door Mondu worden gefactureerd.',
+    'MONDU_CANCEL_INVOICE_ERROR'   => 'Factuur van deze bestelling kan niet door Mondu worden geannuleerd.',
+    'MONDU_CANCEL_ORDER_ERROR'     => 'Bestelling kan niet door Mondu worden geannuleerd.',
+    'MONDU_ADJUST_ORDER_ERROR'     => 'Bestelling kan niet worden aangepast door Mondu.',
+    'MONDU_REGISTER_WEBHOOK_ERROR' => 'Webhook kan niet door Mondu worden gemaakt.'
+);
