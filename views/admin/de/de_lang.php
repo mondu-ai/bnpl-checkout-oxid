3213cd58755afa4d9da3914987342ba54f257400
Dzana Feratovic <80393754+dzanaf@users.noreply.github.com>
2023-01-23 11:32:45 +0100

    [CON-768] NL translations (#5)
    * [CON-768] NL translations
    
    * -update all translations

--- views/admin/de/de_lang.php
+++ views/admin/de/de_lang.php
@@ -6,15 +6,15 @@ $aLang = array(
     'charset'                            => 'UTF-8',
     'SHOP_MODULE_GROUP_oemondu_banners'  => 'Moduleinstellungen',
     'SHOP_MODULE_oemonduApiKey'          => 'API Key',
-    'SHOP_MODULE_oemonduSandboxMode'     => 'Sandbox mode',
+    'SHOP_MODULE_oemonduSandboxMode'     => 'Sandbox Modus',
     'SHOP_MODULE_oemonduErrorLogging'    => 'Mondu Logging aktivieren',
 
     'INVALID_API_KEY'              => 'Ungültiger Mondu API Key.',
     'MONDU_WILL_CREATE_INVOICE'    => 'Beim Versand dieser Bestellung wird eine Rechnung in Mondu erstellt. Möchten Sie fortfahren?',
-    'MONDU_WILL_CANCEL_INVOICE'    => 'Durch die Stornierung des Versands einer Bestellung wird eine Rechnung in Mondu storniert. Diese Aktion kann nicht rückgängig gemacht werden. Sind Sie sicher, dass Sie fortfahren möchten?',
-    'MONDU_CREATE_INVOICE_ERROR'   => 'Diese Bestellung kann von Mondu nicht in Rechnung gestellt werden.',
-    'MONDU_CANCEL_INVOICE_ERROR'   => 'Die Rechnung dieser Bestellung kann von Mondu nicht storniert werden.',
-    'MONDU_CANCEL_ORDER_ERROR'     => 'Die Bestellung kann von Mondu nicht storniert werden.',
-    'MONDU_ADJUST_ORDER_ERROR'     => 'Die Bestellung kann von Mondu nicht angepasst werden.',
-    'MONDU_REGISTER_WEBHOOK_ERROR' => 'Webhook kann nicht von Mondu erstellt werden.'
+    'MONDU_WILL_CANCEL_INVOICE'    => 'Durch die Stornierung des Versands wird eine Rechnung in Mondu storniert. Diese Aktion kann nicht rückgängig gemacht werden. Sind Sie sicher, dass Sie fortfahren möchten?',
+    'MONDU_CREATE_INVOICE_ERROR'   => 'Diese Bestellung kann durch Mondu nicht in Rechnung gestellt werden.',
+    'MONDU_CANCEL_INVOICE_ERROR'   => 'Die Rechnung dieser Bestellung kann durch Mondu nicht storniert werden.',
+    'MONDU_CANCEL_ORDER_ERROR'     => 'Die Bestellung kann durch Mondu nicht storniert werden.',
+    'MONDU_ADJUST_ORDER_ERROR'     => 'Die Bestellung kann durch Mondu nicht angepasst werden.',
+    'MONDU_REGISTER_WEBHOOK_ERROR' => 'Webhook kann nicht durch Mondu erstellt werden.'
 );
