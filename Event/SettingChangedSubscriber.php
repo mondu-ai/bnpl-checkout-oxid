200c3e0ba29b4e6e67ab094386b63e41823f5c3c
Tigran <tiko.hovhannisyan.and.t@gmail.com>
2024-02-28 18:59:46 +0400

    [PT-1105] fix SettingChangedSubscriber triggering on other module settings save

--- Event/SettingChangedSubscriber.php
+++ Event/SettingChangedSubscriber.php
@@ -26,6 +26,8 @@ class SettingChangedSubscriber extends AbstractShopAwareEventSubscriber
 
     public function afterSettingChange(SettingChangedEvent $event)
     {
+        if($event->getModuleId() !== 'oemondu') return;
+
         if (
             $event->getSettingName() === 'oemonduWebhookSecret' ||
             $event->getSettingName() === 'oemonduIsMerchantIdentified'
@@ -52,6 +54,8 @@ class SettingChangedSubscriber extends AbstractShopAwareEventSubscriber
         foreach (self::REQUIRED_WEBHOOK_TOPICS as $webhookTopic) {
             $webhookParams = oxNew(Webhook::class, $webhookTopic)->getData();
             $response = $this->_client->registerWebhook($webhookParams);
+
+            if ($response['status'] === 409) return;
 
             if (!$response['webhook']) {
                 $errorMessage = $response['status'] === 403 ? 'INVALID_API_KEY' : 'MONDU_REGISTER_WEBHOOK_ERROR';
